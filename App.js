f8379a3ce461176a183f944879a01c9467faa725
Spencer Tilton <tiltospe@hotmail.com>
2016-09-06 20:51:47 -0700

    tutorial 5

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 class App extends React.Component {
-    constructor () {
+    constructor() {
         super();
         this.state = {
             txt: 'this is the state txt',
             cat: 0
         }
     }
-    update(e){
-        this.setState({txt: e.target.value})
+    update(e) {
+        this.setState({ txt: e.target.value })
     }
     render() {
         return (
             <div>
-            <input type="text"
-                onChange={this.update.bind(this)} />
-            <h1>{this.state.txt}</h1>
+                <input type="text"
+                    onChange={this.update.bind(this) } />
+                <h1>{this.state.txt}</h1>
             </div>
         );
     }
